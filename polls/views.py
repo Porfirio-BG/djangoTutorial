17fabd9511e0db983279c66facc2f383a1744144
Porfirio-BG <b.fillo.p@gmail.com>
2018-10-14 19:20:29 -0500

    Updated detail view, changed url patterns to namespace them

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
 from .models import Question
-from django.template import loader
-from django.http.response import Http404
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -14,10 +12,7 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
